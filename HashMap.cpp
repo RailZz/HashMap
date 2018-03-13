bc598f84e93ad90923f2272cdf6b5f0606d83ed7
RailZz <trufanov.p@gmail.com>
2018-03-14 00:15:28 +0300

    Update HashMap.cpp

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,49 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <initializer_list>
-#include <stdexcept>
-#include <utility>
 #include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <stdexcept>
+#include <vector>
+#include <utility>
 
-template<class KeyType, class ValueType, class Hash = std::hash<KeyType> > class HashMap {
+template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
+class HashMap {
 public:
-    HashMap(Hash hasher_ = Hash()):hasher(hasher_) {
-        setSize(HashMapSize);
+    HashMap(Hash _hasher = Hash{}) : hasher_(_hasher) {
+        real_size_ = 0;
+        setOptimalSize();
+        resize(current_size_);
     }
+
     template<class Iter>
-    HashMap(Iter begin, Iter end, Hash hasher_ = Hash()):HashMap(hasher_) {
-        setSize(HashMapSize);
+    HashMap(Iter begin, Iter end, Hash _hasher = Hash{}) : HashMap(_hasher) {
+        real_size_ = 0;
         for (auto it = begin; it != end; ++it) {
             data_.push_back(*it);
+            real_size_++;
         }
+        setOptimalSize();
+        resize(current_size_);
         setIterators();
     }
-    HashMap(std::initializer_list<std::pair<const KeyType, ValueType>> init_list, Hash hasher_ = Hash()) :HashMap(init_list.begin(), init_list.end(), hasher_) {}
+
+    HashMap(std::initializer_list<std::pair<const KeyType, ValueType>> init_list,
+        Hash _hasher = Hash{})
+        : HashMap(init_list.begin(), init_list.end(), _hasher) {}
 
     size_t size() const {
-        return data_.size();
+        return real_size_;
     }
+
     bool empty() const {
-        return data_.empty();
+        return real_size_ == 0;
     }
 
     Hash hash_function() const {
-        return hasher;
+        return hasher_;
     }
 
     void insert(const std::pair<const KeyType, ValueType>& add) {
         if (find(add.first) == end()) {
+            real_size_++;
             data_.push_front(add);
-            remainers_[getPos(add.first)].push_back(data_.begin());
+            if (size() == current_size_ * 2) {
+                current_size_ *= 2;
+                resize(current_size_);
+                setIterators();
+            } else {
+                buckets_[getPos(add.first)].push_back(data_.begin());
+            }
         }
     }
+
     void erase(const KeyType& del) {
         size_t pos = getPos(del);
-        for (auto it = remainers_[pos].begin(); it != remainers_[pos].end(); ++it) {
+        for (auto it = buckets_[pos].begin(); it != buckets_[pos].end(); ++it) {
             if ((*it)->first == del) {
+                real_size_--;
                 data_.erase(*it);
-                remainers_[pos].erase(it);
+                buckets_[pos].erase(it);
                 break;
             }
         }
@@ -55,19 +75,22 @@ public:
     iterator begin() {
         return data_.begin();
     }
+
     iterator end() {
         return data_.end();
     }
+
     const_iterator begin() const {
         return data_.begin();
     }
+
     const_iterator end() const {
         return data_.end();
     }
 
     iterator find(const KeyType& key) {
         size_t pos = getPos(key);
-        for (auto it = remainers_[pos].begin(); it != remainers_[pos].end(); ++it) {
+        for (auto it = buckets_[pos].begin(); it != buckets_[pos].end(); ++it) {
             if ((*it)->first == key) {
                 return *it;
             }
@@ -77,7 +100,7 @@ public:
 
     const_iterator find(const KeyType& key) const {
         size_t pos = getPos(key);
-        for (auto it = remainers_[pos].begin(); it != remainers_[pos].end(); ++it) {
+        for (auto it = buckets_[pos].begin(); it != buckets_[pos].end(); ++it) {
             if ((*it)->first == key) {
                 return *it;
             }
@@ -88,13 +111,12 @@ public:
     ValueType& operator[](const KeyType& key) {
         iterator f = find(key);
         if (f == end()) {
-            data_.push_front({ key, ValueType() });
-            size_t pos = getPos(key);
-            remainers_[pos].push_back(data_.begin());
-            return data_.begin()->second;
+            insert({ key, ValueType{} });
+            f = find(key);
         }
         return f->second;
     }
+
     const ValueType& at(const KeyType& key) const {
         const_iterator f = find(key);
         if (f == end()) {
@@ -105,44 +127,54 @@ public:
 
     void clear() {
         for (auto& el : data_) {
-            remainers_[getPos(el.first)].clear();
+            buckets_[getPos(el.first)].clear();
         }
+        real_size_ = 0;
         data_.clear();
+        setOptimalSize();
+        resize(current_size_);
     }
 
     HashMap& operator =(const HashMap& other) {
         if (this != &other) {
-            data_.clear();
-            for (auto elem : other.data_) {
-                data_.push_back(elem);
+            clear();
+            real_size_ = 0;
+            for (auto it = other.begin(); it != other.end(); ++it) {
+                data_.push_back(*it);
+                real_size_++;
             }
-            setSize(HashMapSize);
+            setOptimalSize();
+            resize(current_size_);
             setIterators();
         }
         return *this;
     }
 
-    void print() const {
-        for (auto it = data_.begin(); it != data_.end(); ++it) {
-            std::cout << "(" << it->first << ", " << it->second << ") ";
-        }
-        std::cout << std::endl;
+private:
+    size_t real_size_;
+    size_t current_size_;
+    std::list<std::pair<const KeyType, ValueType>> data_;
+    std::vector<std::list<iterator>> buckets_;
+    Hash hasher_;
+
+    inline void resize(size_t size) {
+        buckets_.assign(size, {});
     }
 
-private:
-    const int HashMapSize = 100000;
-    std::list<std::pair<const KeyType, ValueType>> data_;
-    std::vector<std::list<iterator>> remainers_;
-    Hash hasher;
-    void setSize(size_t size) {
-        remainers_ = std::vector<std::list<iterator>>(size, std::list<iterator>());
+    inline void setOptimalSize() {
+        current_size_ = 1;
+        while (current_size_ <= real_size_) {
+            current_size_ <<= 1;
+        }
     }
-    size_t getPos(const KeyType& elem) const {
-        return hasher(elem) % HashMapSize;
+
+    inline size_t getPos(const KeyType& elem) const {
+        return hasher_(elem) % current_size_;
     }
+
     void setIterators() {
         for (auto it = data_.begin(); it != data_.end(); ++it) {
-            remainers_[getPos(it->first)].push_back(it);
+            buckets_[getPos(it->first)].push_back(it);
         }
     }
 };
